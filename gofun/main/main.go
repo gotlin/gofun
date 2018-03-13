6d3c2822da3d70927007fe8ed64df38e9d687dd8
forrestlin <forrestlin@lexinfintech.com>
2018-03-13 18:26:03 +0800

    调整目录

--- gofun/main/main.go
+++ gofun/main/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"gofun/letcode/LongestCommonPrefix"
- )
+	"io"
+	"log"
+	"net"
+	"time"
+)
 
 func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	strs:=make([]string,1)
-	strs = append(strs,"a")
-	strs = append(strs,"ab")
-	strs = append(strs,"abc")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		handleConn(conn) // handle one connection at a time
+	}
+}
 
-	res:=LongestCommonPrefix.LongestCommonPrefix(strs)
-	fmt.Println(res)
-
-
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
